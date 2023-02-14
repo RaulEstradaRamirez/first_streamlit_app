28eea5b68ceaf0a4e72f7af55e77b9e0f1f66521
RaulEstradaRamirez <124305641+RaulEstradaRamirez@users.noreply.github.com>
2023-02-13 18:12:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,10 @@ import streamlit as st
 
 title = st.text_input('What fruit would you like information about?','kiwi')
 st.write('The current movie title is', title)
-streamlit.dataframe(title)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),title)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+# Display the table on the page
+streamlit.dataframe(fruits_to_show)
 
 
 
