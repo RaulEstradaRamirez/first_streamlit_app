1dd71e162471823825776b397853e2926f3b1664
RaulEstradaRamirez <124305641+RaulEstradaRamirez@users.noreply.github.com>
2023-02-15 13:10:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,12 +27,12 @@ streamlit.text('What fruit would you like information about?')
 title = streamlit.text_input('What fruit would you like information about?','kiwi')
 streamlit.write('The current movie title is', title)
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+
+my_fruit_list2 = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list2 = my_fruit_list2.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list2.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list2.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
