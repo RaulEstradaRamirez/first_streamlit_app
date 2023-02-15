36bacc077509ee3f8296c3c7b19d59d86ec84f37
RaulEstradaRamirez <124305641+RaulEstradaRamirez@users.noreply.github.com>
2023-02-15 17:42:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,12 @@ streamlit.dataframe(fruits_selected)
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+#New Section to display fruityvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
 import snowflake.connector
 
 streamlit.header('Fruityvice Fruit Advice!')
